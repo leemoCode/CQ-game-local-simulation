0e0d615705782cf5cd51866e5ef63c44f92e98c8
leemoCode <73377587+leemoCode@users.noreply.github.com>
2021-10-22 17:20:59 +0800

    refactor: remove uselss code

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <component :is="curPage"></component>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
+export default defineComponent({
+  name: 'App',
   components: {
     HelloWorld,
   },
-})
-export default class App extends Vue {}
-</script>
+  setup() {
+    const curPage = ref('HelloWorld');
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    return {
+      curPage,
+    };
+  },
+});
+</script>
