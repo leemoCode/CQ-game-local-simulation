e769ffb095795feb29b6e34dbd572ce035922fe5
leemoCode <73377587+leemoCode@users.noreply.github.com>
2021-10-25 10:14:11 +0800

    feat: add leetCode area

---
+++ src/leetCode/239.js
@@ -0,0 +1,26 @@
+const solution = (nums, k) => {
+  let deque = [];
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i - k >= deque[0]) {
+      deque.shift();
+    }
+
+    while (nums[deque[deque.length - 1]] <= nums[i]) {
+      deque.pop();
+    }
+
+    deque.push(i);
+
+    if (i >= k - 1) {
+      result.push(nums[deque[0]]);
+    }
+  }
+
+  console.log(result);
+
+  return result;
+};
+
+solution([9, 11], 2);
