bf919c6bc91200da5cd20147d08a7cfd4d8d6c61
leemoCode <L777888@88.com>
2021-11-16 20:39:58 +0800

    ud todo

---
+++ src/leetCode/234.js
@@ -0,0 +1,16 @@
+const solution = (head) => {
+  let left = head;
+  const solutionCurGroup = (right) => {
+    if (right === null) {
+      return true;
+    }
+    const resLast = solutionCurGroup(right.next);
+    const curRes = resLast && left.val === right.val;
+    left = left.next;
+    return curRes;
+  };
+
+  return solutionCurGroup(head);
+};
+
+solution();
